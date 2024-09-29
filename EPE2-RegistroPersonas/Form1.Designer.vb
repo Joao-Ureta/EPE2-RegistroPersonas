8385cc96947eddeb85081dbed8fca160e0bb166b
Joao Ureta <joao.ureta@gmail.com>
2024-09-29 01:03:18 -0300

    Agregar archivos de proyecto.

---
+++ EPE2-RegistroPersonas/Form1.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtRut = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.cbxComuna = New System.Windows.Forms.ComboBox()
+        Me.txtObservacion = New System.Windows.Forms.TextBox()
+        Me.txtCiudad = New System.Windows.Forms.TextBox()
+        Me.txtApellidos = New System.Windows.Forms.TextBox()
+        Me.txtNombres = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.rBtnNulo = New System.Windows.Forms.RadioButton()
+        Me.rBtnFemenino = New System.Windows.Forms.RadioButton()
+        Me.rBtnMasculino = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(76, 165)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(453, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sistema de registro de personas"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DarkGray
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.btnLimpiar)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.txtRut)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(12, 223)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(582, 84)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(336, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtRut
+        '
+        Me.txtRut.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRut.Location = New System.Drawing.Point(105, 23)
+        Me.txtRut.Multiline = True
+        Me.txtRut.Name = "txtRut"
+        Me.txtRut.Size = New System.Drawing.Size(212, 29)
+        Me.txtRut.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(32, 23)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 29)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Rut:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(36, 213)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Busqueda:"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DarkGray
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.btnGuardar)
+        Me.Panel2.Controls.Add(Me.cbxComuna)
+        Me.Panel2.Controls.Add(Me.txtObservacion)
+        Me.Panel2.Controls.Add(Me.txtCiudad)
+        Me.Panel2.Controls.Add(Me.txtApellidos)
+        Me.Panel2.Controls.Add(Me.txtNombres)
+        Me.Panel2.Controls.Add(Me.Label10)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Location = New System.Drawing.Point(12, 333)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(582, 461)
+        Me.Panel2.TabIndex = 3
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Enabled = False
+        Me.btnGuardar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGuardar.ForeColor = System.Drawing.Color.Black
+        Me.btnGuardar.Image = Global.EPE2_RegistroPersonas.My.Resources.Resources.guardar
+        Me.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnGuardar.Location = New System.Drawing.Point(199, 360)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(178, 53)
+        Me.btnGuardar.TabIndex = 12
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'cbxComuna
+        '
+        Me.cbxComuna.Enabled = False
+        Me.cbxComuna.FormattingEnabled = True
+        Me.cbxComuna.Location = New System.Drawing.Point(193, 205)
+        Me.cbxComuna.Name = "cbxComuna"
+        Me.cbxComuna.Size = New System.Drawing.Size(243, 24)
+        Me.cbxComuna.TabIndex = 11
+        '
+        'txtObservacion
+        '
+        Me.txtObservacion.Enabled = False
+        Me.txtObservacion.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtObservacion.Location = New System.Drawing.Point(193, 290)
+        Me.txtObservacion.Multiline = True
+        Me.txtObservacion.Name = "txtObservacion"
+        Me.txtObservacion.Size = New System.Drawing.Size(351, 29)
+        Me.txtObservacion.TabIndex = 10
+        '
+        'txtCiudad
+        '
+        Me.txtCiudad.Enabled = False
+        Me.txtCiudad.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCiudad.Location = New System.Drawing.Point(193, 248)
+        Me.txtCiudad.Multiline = True
+        Me.txtCiudad.Name = "txtCiudad"
+        Me.txtCiudad.Size = New System.Drawing.Size(243, 29)
+        Me.txtCiudad.TabIndex = 9
+        '
+        'txtApellidos
+        '
+        Me.txtApellidos.Enabled = False
+        Me.txtApellidos.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtApellidos.Location = New System.Drawing.Point(193, 67)
+        Me.txtApellidos.Multiline = True
+        Me.txtApellidos.Name = "txtApellidos"
+        Me.txtApellidos.Size = New System.Drawing.Size(351, 29)
+        Me.txtApellidos.TabIndex = 8
+        '
+        'txtNombres
+        '
+        Me.txtNombres.Enabled = False
+        Me.txtNombres.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombres.Location = New System.Drawing.Point(193, 27)
+        Me.txtNombres.Multiline = True
+        Me.txtNombres.Name = "txtNombres"
+        Me.txtNombres.Size = New System.Drawing.Size(351, 29)
+        Me.txtNombres.TabIndex = 7
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(32, 290)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(155, 29)
+        Me.Label10.TabIndex = 6
+        Me.Label10.Text = "Observacion:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(32, 248)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(96, 29)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = "Ciudad:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(32, 205)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(109, 29)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Comuna:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(44, 107)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(43, 17)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Sexo:"
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Silver
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel3.Controls.Add(Me.rBtnNulo)
+        Me.Panel3.Controls.Add(Me.rBtnFemenino)
+        Me.Panel3.Controls.Add(Me.rBtnMasculino)
+        Me.Panel3.Location = New System.Drawing.Point(14, 116)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(548, 73)
+        Me.Panel3.TabIndex = 3
+        '
+        'rBtnNulo
+        '
+        Me.rBtnNulo.AutoSize = True
+        Me.rBtnNulo.Enabled = False
+        Me.rBtnNulo.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rBtnNulo.Location = New System.Drawing.Point(347, 18)
+        Me.rBtnNulo.Name = "rBtnNulo"
+        Me.rBtnNulo.Size = New System.Drawing.Size(181, 33)
+        Me.rBtnNulo.TabIndex = 2
+        Me.rBtnNulo.TabStop = True
+        Me.rBtnNulo.Text = "No especifica"
+        Me.rBtnNulo.UseVisualStyleBackColor = True
+        '
+        'rBtnFemenino
+        '
+        Me.rBtnFemenino.AutoSize = True
+        Me.rBtnFemenino.Enabled = False
+        Me.rBtnFemenino.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rBtnFemenino.Location = New System.Drawing.Point(183, 18)
+        Me.rBtnFemenino.Name = "rBtnFemenino"
+        Me.rBtnFemenino.Size = New System.Drawing.Size(143, 33)
+        Me.rBtnFemenino.TabIndex = 1
+        Me.rBtnFemenino.TabStop = True
+        Me.rBtnFemenino.Text = "Femenino"
+        Me.rBtnFemenino.UseVisualStyleBackColor = True
+        '
+        'rBtnMasculino
+        '
+        Me.rBtnMasculino.AutoSize = True
+        Me.rBtnMasculino.Enabled = False
+        Me.rBtnMasculino.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rBtnMasculino.Location = New System.Drawing.Point(21, 18)
+        Me.rBtnMasculino.Name = "rBtnMasculino"
+        Me.rBtnMasculino.Size = New System.Drawing.Size(143, 33)
+        Me.rBtnMasculino.TabIndex = 0
+        Me.rBtnMasculino.TabStop = True
+        Me.rBtnMasculino.Text = "Masculino"
+        Me.rBtnMasculino.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(32, 67)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(120, 29)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Apellidos:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(32, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(119, 29)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Nombres:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(36, 323)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 17)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Persona:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.EPE2_RegistroPersonas.My.Resources.Resources.IPCHILE
+        Me.PictureBox1.Location = New System.Drawing.Point(109, 17)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(398, 126)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.ForeColor = System.Drawing.Color.Black
+        Me.btnLimpiar.Location = New System.Drawing.Point(456, 23)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(106, 29)
+        Me.btnLimpiar.TabIndex = 3
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(606, 827)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Joao Ureta - EVA 2"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtRut As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents rBtnNulo As RadioButton
+    Friend WithEvents rBtnFemenino As RadioButton
+    Friend WithEvents rBtnMasculino As RadioButton
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtCiudad As TextBox
+    Friend WithEvents txtApellidos As TextBox
+    Friend WithEvents txtNombres As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtObservacion As TextBox
+    Friend WithEvents cbxComuna As ComboBox
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnLimpiar As Button
+End Class
---
+++ EPE2-RegistroPersonas/Form1.vb
@@ -0,0 +1,190 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim connectionString As String = "server=localhost;userid=jUreta;password=Maju2223;database=registropersonas"
+    Private connection As MySqlConnection
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            'instancia de conexion
+            connection = New MySqlConnection(connectionString)
+
+            connection.Open()
+
+            'funcion para cargar comunas
+            CargarComunas()
+
+        Catch ex As MySqlException
+            'en caso de falla de conexion muestra mensaje de error
+            MessageBox.Show("Error al conectar a la base de datos: " & ex.Message)
+        Finally
+            If connection IsNot Nothing Then connection.Close()
+        End Try
+    End Sub
+
+    Private Sub CargarComunas()
+        Try
+            'consulta para obtener las comunas de la base de datos
+            Dim query As String = "SELECT NombreComuna FROM comuna"
+            Dim command As New MySqlCommand(query, connection)
+
+            'ejecuta y lee la consulta
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+
+            'limpia el combobox
+            cbxComuna.Items.Clear()
+
+            'agregar "seleccione una comuna" como opcion
+            cbxComuna.Items.Add("Seleccione una comuna")
+
+            'agrega las comunas al combobox
+            While reader.Read()
+                cbxComuna.Items.Add(reader("NombreComuna").ToString())
+            End While
+
+            reader.Close()
+
+            'para dejar como primer campo "seleccione comuna"
+            cbxComuna.SelectedIndex = 0
+
+        Catch ex As MySqlException
+            'mensaje para mostrar error en caso de consulta fallida
+            MessageBox.Show("Error en la consulta: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            'instancia de conexion
+            connection = New MySqlConnection(connectionString)
+            connection.Open()
+
+            'consulta para buscar los datos de la tabla personas
+            Dim query As String = "SELECT Nombre, Apellido, Sexo, Comuna, Ciudad, Observacion FROM personas WHERE RUT = @RUT"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@RUT", txtRut.Text)
+
+            'ejecuta la consulta
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+
+            If reader.HasRows Then
+                'si el rut existe, se cargan los datos en los campos
+                While reader.Read()
+                    txtNombres.Text = reader("Nombre").ToString()
+                    txtApellidos.Text = reader("Apellido").ToString()
+                    cbxComuna.Text = reader("Comuna").ToString()
+                    txtCiudad.Text = reader("Ciudad").ToString()
+                    txtObservacion.Text = reader("Observacion").ToString()
+
+                    'metodo para asignar el dato al radioButton
+                    Select Case reader("Sexo").ToString()
+                        Case "Masculino"
+                            rBtnMasculino.Checked = True
+                        Case "Femenino"
+                            rBtnFemenino.Checked = True
+                        Case Else
+                            rBtnNulo.Checked = True
+                    End Select
+                End While
+
+                'mantiene los campos bloqueados si el rut existe
+                DesbloquearCampos(False)
+            Else
+                'si el rut no existe muestra un msgBox de opciones
+                Dim resultado As DialogResult
+                resultado = MessageBox.Show("RUT no encontrado. ¿Desea ingresar un nuevo registro?", "Confirmar Nuevo Registro", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+                If resultado = DialogResult.Yes Then
+                    'si se da opcion a si, se limpia y desbloquean los campos para nuevo ingreso
+                    LimpiarCampos()
+                    DesbloquearCampos(True)
+                End If
+            End If
+
+
+            reader.Close()
+        Catch ex As MySqlException
+            MessageBox.Show("Error al conectar a la base de datos: " & ex.Message)
+        Finally
+            'cierre de conexion
+            If connection IsNot Nothing Then connection.Close()
+        End Try
+    End Sub
+
+    'funcion para desbloquear campos si el rut no existe
+    Private Sub DesbloquearCampos(estado As Boolean)
+        txtNombres.Enabled = estado
+        txtApellidos.Enabled = estado
+        rBtnMasculino.Enabled = estado
+        rBtnFemenino.Enabled = estado
+        rBtnNulo.Enabled = estado
+        cbxComuna.Enabled = estado
+        txtCiudad.Enabled = estado
+        txtObservacion.Enabled = estado
+        btnGuardar.Enabled = estado
+    End Sub
+
+    'funcion para limpiar campos si el rut no existe
+    Private Sub LimpiarCampos()
+        txtNombres.Text = ""
+        txtApellidos.Text = ""
+        rBtnMasculino.Checked = False
+        rBtnFemenino.Checked = False
+        rBtnNulo.Checked = False
+        cbxComuna.Text = ""
+        txtCiudad.Text = ""
+        txtObservacion.Text = ""
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        LimpiarCampos2()  'función para limpiar todos los campos e incluir rut
+    End Sub
+
+    'funcion de limpiar campos incluido rut
+    Private Sub LimpiarCampos2()
+        txtNombres.Text = ""
+        txtApellidos.Text = ""
+        rBtnMasculino.Checked = False
+        rBtnFemenino.Checked = False
+        rBtnNulo.Checked = False
+        cbxComuna.Text = ""
+        txtCiudad.Text = ""
+        txtObservacion.Text = ""
+        txtRut.Text = ""
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+            'instancia de conexion
+            connection = New MySqlConnection(connectionString)
+            connection.Open()
+
+            'consulta de ingreso de datos
+            Dim query As String = "INSERT INTO personas (RUT, Nombre, Apellido, Sexo, Comuna, Ciudad, Observacion) VALUES (@RUT, @Nombre, @Apellido, @Sexo, @Comuna, @Ciudad, @Observacion)"
+            Dim command As New MySqlCommand(query, connection)
+
+            'asigna los datos a cada campo
+            command.Parameters.AddWithValue("@RUT", txtRut.Text)
+            command.Parameters.AddWithValue("@Nombre", txtNombres.Text)
+            command.Parameters.AddWithValue("@Apellido", txtApellidos.Text)
+            command.Parameters.AddWithValue("@Sexo", If(rBtnMasculino.Checked, "Masculino", If(rBtnFemenino.Checked, "Femenino", "Nulo")))
+            command.Parameters.AddWithValue("@Comuna", cbxComuna.Text)
+            command.Parameters.AddWithValue("@Ciudad", txtCiudad.Text)
+            command.Parameters.AddWithValue("@Observacion", txtObservacion.Text)
+
+            'ejecuta la consulta
+            command.ExecuteNonQuery()
+
+            MessageBox.Show("Registro guardado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            LimpiarCampos2() 'limpia los campos despues de un ingreso exitoso
+            DesbloquearCampos(False) 'vuelve a bloquear los campos despues de ingreso exitoso
+
+
+        Catch ex As MySqlException
+            'manejo de error en caso de falla de ingreso
+            MessageBox.Show("Error al guardar el registro: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If connection IsNot Nothing Then connection.Close()
+        End Try
+    End Sub
+End Class
